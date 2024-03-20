42f67b811bdbcf26bf51ba701493be1766855e99
SHALINI A R K <157042601+SHALINI-ARK@users.noreply.github.com>
2024-03-20 22:46:23 +0530

    Bank

---
+++ ATM INTERFACE/Bank.java
@@ -0,0 +1,18 @@
+public class Bank {
+	  private int balance;
+	    public Bank(int balance) {
+	        this.balance = balance;
+	    }
+	    public void withdraw(int amt){
+	        if (balance < amt) {
+	           System.out.println("Insufficient fund");
+	        }
+	        balance -= amt;
+	    }
+	    public void deposit(int amt) {
+	        balance += amt;
+	    }
+	    public int getBalance() {
+	        return balance;
+	    }
+}
